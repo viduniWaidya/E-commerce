d2f84ed5c1aa56dbf8e06ff324df305cf6bff990
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-08-24 20:28:02 +0530

    Update

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import {useParams} from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 function Product() {
+  const {all_product}= useContext(ShopContext);
+  const {ProductId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(ProductId));
   return (
     <div>
-      
+      <Breadcrum product={product} />
     </div>
   )
 }
