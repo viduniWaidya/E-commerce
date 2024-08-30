0981b94857c6c8a6795ddbdb381b9f8dd7d072e4
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-08-30 23:56:20 +0530

    update

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,19 +1,21 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
-import {useParams} from 'react-router-dom';
-import Breadcrum from '../Components/Breadcrums/Breadcrum';
-import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
 
-function Product() {
+
+
+const Product = () => {
   const {all_product}= useContext(ShopContext);
-  const {ProductId} = useParams();
-  const product = all_product.find((e)=> e.id === Number(ProductId));
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId))
   return (
     <div>
-      <Breadcrum product={product} />
-      <ProductDisplay product= {product}/>
+      <Breadcrum product={product}/>
     </div>
   )
 }
 
 export default Product
+
