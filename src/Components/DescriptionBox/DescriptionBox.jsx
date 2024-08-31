331486067cb03edc0e197666e55a6fd7c7d50ecd
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-08-31 21:51:10 +0530

    update

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className='descriptionbox-navigator'>
+        <div>2.14.01</div>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
