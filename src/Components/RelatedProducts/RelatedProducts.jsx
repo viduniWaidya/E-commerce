0a20e79cce12ac35d43e7b26d6a809e47d2b6741
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-09-02 00:11:54 +0530

    update

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './RelatedProducts.css'
+import data_product from '../Assets/data'
+import Item from '../Item/Item'
 
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
-      <h1>Telated Products</h1>
+      <h1>Related Products</h1>
       <hr />
-      <div className='relatedproducts-item'></div>
+      <div className='relatedproducts-item'>
+        {data_product.map((item,i)=>{
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price }/>
+        })}
+      </div>
     </div>
   )
 }
 
 export default RelatedProducts
+
+//2.22.28
