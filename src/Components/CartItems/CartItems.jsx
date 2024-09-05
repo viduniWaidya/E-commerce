f2bb34c71d5ed64cb235ef614db6f8b13dec162f
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-09-05 23:53:47 +0530

    update

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -16,17 +16,22 @@ const CartItems = () => {
         <p>Remove</p>
       </div>
       <hr />
-      <div>
-        <div className='cartitems-farmat'>
-          <img src='' alt='' className='carticon-product-icon' />
-          <p></p>
-          <p></p>
-          <button className='cartitems-quantity'></button>
-          <p></p>
-          <img src={remove_icon} onClick={()=>{removeFromCart()}} alt='' />
-        </div>
-        <hr />
-      </div>
+      {all_product.map((e)=>{
+        if(cartItems[e.id]>0)
+        {
+          return <div>
+                    <div className='cartitems-farmat'>
+                      <img src={e.image} alt='' className='carticon-product-icon' />
+                      <p>{e.name}</p>
+                      <p>${e.new_price}</p>
+                      <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                      <p>{e.new_price*cartItems[e.id]}</p>
+                      <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt='' />
+                    </div>
+                    <hr />
+                  </div>
+        }
+      })}
     </div>
   )
 }
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -17,6 +17,7 @@ const ShopContextProvider = (props) => {
     
     const addToCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
     }
 
     const removeFromCart = (itemId) => {
