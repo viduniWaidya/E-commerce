f44cd6e9e2d52b2e14c8436450787ebd791e3d47
Viduni_Waidyaratne <160768171+viduniWaidya@users.noreply.github.com>
2024-09-13 22:21:32 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
-function Navbar() {
+const Navbar = () => {
 
 const [menu,setMenu] = useState("shop")
+const {getTotalCartItems} = useContext(ShopContext);
+const menuRef = useRef();
+
+const dropdown_toggle = (e) =>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+}
 
   return (
     <div className='navbar'>
@@ -15,7 +24,8 @@ const [menu,setMenu] = useState("shop")
         <img src={logo} alt='' />
         <p>SHOPPER</p>
       </div>
-      <ul className='nav-menu'>
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt='' />
+      <ul ref={menuRef} className='nav-menu'>
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -24,7 +34,7 @@ const [menu,setMenu] = useState("shop")
       <div className='nav-login-cart'>
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt='' /></Link>
-        <div className='nav-cart-count'>0</div>
+        <div className='nav-cart-count'>{getTotalCartItems()}</div>
       </div>
     </div>
   )
